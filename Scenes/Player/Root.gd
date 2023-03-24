bf0dace25b177ac7d3b073312adb3e4ae3090aae
Kajetan Pynka <siema1234@gazeta.pl>
2023-03-24 18:26:17 +0100

    Added 4 different spells

--- Scenes/Player/Root.gd
+++ Scenes/Player/Root.gd
@@ -3,10 +3,24 @@ extends Node2D
 @export var bulletSpeed: int = 300
 @export var bulletScene: PackedScene
 
+enum SPELL { NONE, FIRE, WATER, LIGHTNING, EARTH }
+
 func shoot():
-	var shoot = Input.is_action_just_pressed('shoot')
+	var isShot = Input.is_action_just_pressed('shoot')
 	
-	if shoot:
+	if isShot:
+		
+		# idk chyba 4 osobne sceny dla każdego spella
+		# (choć ulepszenia w drzewku też mają to zmieniać to nwm)
+		match $Player.selectedSpell:
+			SPELL.FIRE:
+				# fireBall.instantiate()
+				# position i velocity tak jak niżej i dodawane do sceny
+				# tylko jak z tymi ulepszeniami hmmmmm
+				pass
+			_:
+				pass
+		
 		var newBullet: CharacterBody2D = bulletScene.instantiate()
 		var mousePos = $Player.get_local_mouse_position().normalized()
 		newBullet.position = $Player.position
--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -2,10 +2,35 @@ extends CharacterBody2D
 
 @export var jump_speed: int = -400
 @export var run_speed: int = 300
+@export var joystick_sens: int = 700
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jumping = false
+enum SPELLS { NONE, FIRE, WATER, LIGHTNING, EARTH }
+var selectedSpell: SPELLS = SPELLS.NONE
+
+func select_spell():
+	if Input.is_action_just_pressed("select_fire"):
+		selectedSpell = SPELLS.FIRE if selectedSpell != SPELLS.FIRE else SPELLS.NONE
+	if Input.is_action_just_pressed("select_water"):
+		selectedSpell = SPELLS.WATER if selectedSpell != SPELLS.WATER else SPELLS.NONE
+	if Input.is_action_just_pressed("select_lightning"):
+		selectedSpell = SPELLS.LIGHTNING if selectedSpell != SPELLS.LIGHTNING else SPELLS.NONE
+	if Input.is_action_just_pressed("select_earth"):
+		selectedSpell = SPELLS.EARTH if selectedSpell != SPELLS.EARTH else SPELLS.NONE
+
+func joystick_aim(delta):
+	var direction: Vector2
+	direction.x = Input.get_action_strength("aim_right") - Input.get_action_strength("aim_left")
+	direction.y = Input.get_action_strength("aim_down") - Input.get_action_strength("aim_up")
+	
+	if abs(direction.x) == 1 and abs(direction.y) == 1:
+		direction = direction.normalized()
+
+	var movement = joystick_sens * direction * delta
+	if (movement):  
+		get_viewport().warp_mouse(get_global_mouse_position() + movement) 
 
 func get_input():
 	velocity.x = 0
@@ -22,8 +47,6 @@ func get_input():
 		velocity.x -= run_speed
 
 func _physics_process(delta):
-	get_input()
-	# shoot()
 	velocity.y += gravity * delta
 	if jumping and is_on_floor():
 		jumping = false
@@ -31,6 +54,9 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _process(delta):
+	select_spell()
+	joystick_aim(delta)
+	get_input()
 	$AnimatedSprite2D.flip_h = velocity.x < 0
 
 func _ready():
