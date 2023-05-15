fcca71befac0a237576c0f7f935bb7bd0c6f599e
Kajetan Pynka <siema1234@gazeta.pl>
2023-05-15 23:47:45 +0200

    Added Choron's artifact

--- Scenes/Spells/NormalSpell/normal_spell.gd
+++ Scenes/Spells/NormalSpell/normal_spell.gd
@@ -10,10 +10,11 @@ func _physics_process(delta):
 	
 	for i in get_slide_collision_count():
 		var collision: KinematicCollision2D = get_slide_collision(i)
-		if collision.get_collider().is_class("CharacterBody2D") and not is_queued_for_deletion():
-			var character: CharacterBody2D = collision.get_collider()
+		var collider = collision.get_collider()
+		if collider.is_class("CharacterBody2D") and not is_queued_for_deletion():
+			var character: CharacterBody2D = collider
 			if character.get_collision_layer() == ENEMY_COLLISION_LAYER:
 				character.got_hit(damage, SPELL.NONE)
 				queue_free()
-		if collision.get_collider().is_class("TileMap") and not is_queued_for_deletion():
+		if collider.is_class("TileMap") and not is_queued_for_deletion():
 			queue_free()
